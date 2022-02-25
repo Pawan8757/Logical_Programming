7a7d4f80b788abab38d31e05063254d74158c0fe
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-25 22:41:59 +0530

    adding welcome file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Logical_Programing
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HERE IS SOME LOGICAL PROGRAMMING");
+            //PerfectNumber.FindThePerfectNumber();
+            //Fibonacci.DisplayFibonacci();
+            //Reverse.ReverseTheNumber();
+        }
+    }
+}
